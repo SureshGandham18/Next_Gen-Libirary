d7b63b339226809da0f2faf479ec41de3f823cae
SureshGandham18 <20pa1a0540@vishnu.edu.in>
2024-02-17 20:46:44 +0530

    ......

--- src/pages/BookAvailability.js
+++ src/pages/BookAvailability.js
@@ -29,15 +29,15 @@ function BooksAvailability() {
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-6 mt-20 text-center">BOOKS</h1>
-      <div className="mb-6 flex items-center bg-white px-2 rounded-lg mx-72">
+      <div className="mb-6 flex items-center bg-white px-2 rounded-lg md:w-1/2 lg:w-1/3 mx-auto">
         <span className='text-gray-600'><FaSearch size={20} /></span>
-        <span><input
+        <input
           type="text"
           placeholder="Search your book"
           value={searchTerm}
           onChange={handleSearch}
-          className="w-96 px-4 py-2 border-none  bg-white outline-none"
-        /></span>
+          className="w-full px-4 py-2 border-none bg-white outline-none"
+        />
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {loading ? (
